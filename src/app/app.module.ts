c1f743c48dd2744bda083721bb1a1a982297c033
shorty101 <a.jankovic100@gmail.com>
2017-03-03 22:49:25 +1100

    Getting underway with prototyping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
+import { ArticleComponent } from './article/article.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    SidebarMenuComponent,
+    ArticleComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
